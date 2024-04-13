860ccae73abd0a8c23c0f4ac319892e761af2260
DamianK77 <damik422@gmail.com>
2024-04-13 18:40:26 +0200

    init

---
+++ ESP_CRSF.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "ESP_CRSF.h"
+
+
+void CRSF_init(void)
+{
+
+    //begin uart communication with RX
+    1
+
+}
---
+++ include/ESP_CRSF.h
@@ -0,0 +1,8 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+void CRSF_init(void);
